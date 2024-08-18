0d9a60603e0cf4187d854dd9f72068c61c50c5eb
Maksim <m-zhitkov@inbox.ru>
2024-08-18 17:07:56 +0300

    Implemented the statistics.py module

---
+++ database/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "Base",
+    "Settings"
+)
+
+from .db_conf import Base
+from .models import Settings
---
+++ database/db_conf.py
@@ -0,0 +1,31 @@
+"""We describe the connection to the database."""
+from config import DB_NAME, DB_PASS, DB_USER
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+DATABASE_URL: str = "postgresql+asyncpg://{0}:{1}@localhost:5432/{2}".format(
+    DB_USER,
+    DB_PASS,
+    DB_NAME,
+)
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
+
+
+async def get_async_session() -> AsyncSession:
+    """Function to get a session."""
+    async with async_session_maker() as session:
+        return session
