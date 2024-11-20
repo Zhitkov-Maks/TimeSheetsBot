5c8c0e0a0566501fa365602082294af6a9432387
Maksim <m-zhitkov@inbox.ru>
2024-11-20 19:30:14 +0300

    I've reworked the code, changed the data requests.

--- crud/statistics.py
+++ crud/statistics.py
@@ -1,31 +1,68 @@
+from datetime import datetime
+
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, asc, Sequence
+from sqlalchemy import select, asc, Sequence, Select, func, extract, Row, Result
 from database.db_conf import get_async_session
 from database.models import Salary
 
 
-async def get_information_for_month(user_id: int, year, month) -> Sequence:
-    """Получение данных за выбранный месяц."""
+async def request_statistic(user: int, year: int) -> Row[tuple]:
+    """
+    Запрос на получение небольшой статистики за год.
+    :param user: Id юзера.
+    :param year: Год для статистики.
+    :return: Кортеж со статистикой.
+    """
+    session: AsyncSession = await get_async_session()
+    # Запрос для получения итогового заработка за год
+    stmt: Select = (
+        select(
+            func.sum(Salary.earned),
+            func.sum(Salary.base_hours),
+            func.sum(Salary.overtime),
+        )
+        .where(Salary.user_chat_id == user)
+        .where(
+            (extract("year", func.date(Salary.date)) == year),
+        )
+    )
+    data: Result = await session.execute(stmt)
+    await session.close()
+    return data.one()
+
+
+async def get_information_for_month(user_id: int, year: int, month: int) -> Sequence:
+    """
+    Получение данных за выбранный месяц.
+    :param user_id: Id юзера.
+    :param year: Год.
+    :param month: Месяц.
+    :return: Возвращает данные за месяц, чтобы сформировать календарь.
+    """
     session: AsyncSession = await get_async_session()
     month: str = str(month) if month > 9 else f"0{month}"
 
-    stmt = (select(Salary)
-            .where(Salary.user_chat_id == user_id)
-            .where(Salary.date.ilike(f"%/{month}/{year}%"))
-            .order_by(asc(Salary.date))
-            )
+    stmt: Select = (
+        select(Salary)
+        .where(Salary.user_chat_id == user_id)
+        .where(func.extract("year", Salary.date) == int(year))  # Фильтрация по году
+        .where(func.extract("month", Salary.date) == int(month))
+        .order_by(asc(Salary.date))
+    )
 
-    data = await session.execute(stmt)
+    data: Result = await session.execute(stmt)
     await session.close()
     return data.all()
 
 
 async def get_info_by_date(user_id: int, date: str) -> Salary:
     session: AsyncSession = await get_async_session()
-    stmt = (select(Salary)
-            .where(Salary.user_chat_id == user_id)
-            .where(Salary.date.ilike(date))
-            )
-    data = await session.scalar(stmt)
+    parse_date: date = datetime.strptime(date, "%Y-%m-%d")
+    stmt: Select = (
+        select(Salary)
+        .where(Salary.user_chat_id == user_id)
+        .where(Salary.date == parse_date)
+    )
+    data: Salary = await session.scalar(stmt)
     await session.close()
     return data
