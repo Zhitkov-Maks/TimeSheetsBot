115378cb714ad7cdcd05272a9937cf1200dd0c75
Maksim <m-zhitkov@inbox.ru>
2024-11-20 19:30:58 +0300

    Added a reminder model.

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Float, BIGINT, SMALLINT
+from sqlalchemy import Integer, Float, BIGINT, SMALLINT, Date
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.db_conf import Base
@@ -19,7 +19,7 @@ class Salary(Base):
     base_hours: Mapped[float] = mapped_column(Float)
     overtime: Mapped[float] = mapped_column(Float)
     earned: Mapped[float] = mapped_column(Float)
-    date: Mapped[str] = mapped_column(String)
+    date: Mapped[Date] = mapped_column(Date)
     period: Mapped[int] = mapped_column(Integer, default=1)
 
 
