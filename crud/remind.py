af93e7b58bb324ef6afb1800f693753fd81960af
Maksim <m-zhitkov@inbox.ru>
2024-11-20 19:11:33 +0300

    I've reworked the code, deleted unnecessary code

--- crud/remind.py
+++ crud/remind.py
@@ -6,30 +6,28 @@ from sqlalchemy import (
     Delete,
     ScalarResult,
 )
-from sqlalchemy.exc import IntegrityError, ProgrammingError
+from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.db_conf import get_async_session
 from database.models import Remind
+from config import logger
 
 
 async def add_user_time(time: int, user: int) -> None:
     """
     Добавляет настройки для уведомлений.
     :param time: Новое время уведомлений.
-    :param user: Пользователь у которого нужно обновить время.
+    :param user: ID пользователя телеграм у которого нужно обновить время.
     :return None:
     """
-    session = await get_async_session()
+    session: AsyncSession = await get_async_session()
     remind: Remind = Remind(
         user_chat_id=user,
         time=time,
     )
-    try:
-        session.add(remind)
-        await session.commit()
-    except IntegrityError:
-        pass
+    session.add(remind)
+    await session.commit()
 
 
 async def upgrade_time(time: int, user: int) -> None:
@@ -39,7 +37,7 @@ async def upgrade_time(time: int, user: int) -> None:
     :param user: Пользователь у которого нужно обновить время.
     :return None:
     """
-    session = await get_async_session()
+    session: AsyncSession = await get_async_session()
     stmt: Select = select(Remind).filter(Remind.user_chat_id == user)
     remind: Remind = await session.scalar(stmt)
 
@@ -47,7 +45,7 @@ async def upgrade_time(time: int, user: int) -> None:
         remind.time = time
         await session.commit()
     else:
-        pass
+        logger.error("Function upgrade_time: Напоминание не найдено чтобы менять его.")
 
 
 async def remove_time(user: int) -> None:
@@ -56,33 +54,21 @@ async def remove_time(user: int) -> None:
     :param user: Пользователь которому нужно удалить настройки.
     :return None:
     """
-    session = await get_async_session()
+    session: AsyncSession = await get_async_session()
     stmt: Delete = delete(Remind).where(Remind.user_chat_id == user)
     await session.execute(stmt)
     await session.commit()
 
 
-async def get_settings_all() -> Sequence:
-    """
-    Функция возвращает список пользователей у которых имеются настройки
-    для уведомлений. Код обернут в try except по причине того что при первом
-    запуске проекта база еще не создана, а у нас сразу выполняется запрос
-    на получение пользователей у которых настроено уведомление и приложение
-    падает с ошибкой.
-    :return Sequence: Список пользователей и время для показа уведомлений.
-    """
-    session = await get_async_session()
-    try:
-        stmt = select(Remind)
-        results: ScalarResult = await session.scalars(stmt)
-        return results.all()
-    except ProgrammingError:
-        pass
-
-
 async def get_all_users() -> Sequence:
+    """
+    Функция для получения списка пользователей у которых есть настройка
+    уведомлений.
+    :return: Список напоминаний, в которых есть user_chat_id.
+    """
     session: AsyncSession = await get_async_session()
     stmt: Select = select(Remind)
-    data = await session.scalars(stmt)
+    data: ScalarResult[Remind] = await session.scalars(stmt)
+
     await session.close()
     return data.all()
