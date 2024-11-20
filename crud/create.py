c4b1d45e54983c2b30634394dcb8d2aa430d507d
Maksim <m-zhitkov@inbox.ru>
2024-11-20 18:45:44 +0300

    I've reworked the code

--- crud/create.py
+++ crud/create.py
@@ -1,6 +1,7 @@
-from typing import Tuple
+from datetime import datetime, date
+from typing import Dict
 
-from sqlalchemy import select, func, update, Row, delete
+from sqlalchemy import update, delete, Delete, Update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.db_conf import get_async_session
@@ -8,22 +9,26 @@ from database.models import Salary
 
 
 async def write_salary(
-        base: float,
-        overtime: float,
-        earned: float,
-        data_: dict
+    base: float, overtime: float, earned: float, data_: Dict[str, int | str]
 ) -> None:
-    """Функция для сохранения настроек пользователя."""
+    """
+    Функция для сохранения записи о смене.
+    :param base: Базовая ставка
+    :param overtime: Доплата за переработку.
+    :param earned: Итого заработано.
+    :param data_: Словарь с остальными данными.
+    """
     session: AsyncSession = await get_async_session()
-    period = 1 if int(data_["date"][:2]) <= 15 else 2
+    period: int = 1 if int(data_["date"][:2]) <= 15 else 2
+    parse_date: date = datetime.strptime(data_["date"], "%Y-%m-%d")
 
     data: dict = {
         "user_chat_id": data_["user_id"],
         "base_hours": float(base),
         "overtime": float(overtime),
         "earned": float(earned),
-        "date": data_["date"],
-        "period": period
+        "date": parse_date,
+        "period": period,
     }
 
     salary: Salary = Salary(**data)
@@ -32,94 +37,41 @@ async def write_salary(
 
 
 async def update_salary(
-        base: int,
-        overtime: int,
-        earned: int,
-        data_: dict
+    base: int, overtime: int, earned: int, data_: Dict[str, int | str]
 ) -> None:
-    """Функция для обновления настроек в базе данных."""
+    """
+    Функция для обновления данных о смене в базе данных.
+    :param base: Базовая ставка.
+    :param overtime: Доплата за переработку.
+    :param earned: Итого заработано.
+    :param data_: Словарь с остальными данными.
+    """
     session: AsyncSession = await get_async_session()
+    parse_date: date = datetime.strptime(data_["date"], "%Y-%m-%d")
 
-    stmt = (update(Salary)
-            .where(Salary.user_chat_id == data_["user_id"])
-            .where(Salary.date == data_["date"])
-            .values(base_hours=base, overtime=overtime, earned=earned))
+    stmt: Update = (
+        update(Salary)
+        .where(Salary.user_chat_id == data_["user_id"])
+        .where(Salary.date == parse_date)
+        .values(base_hours=base, overtime=overtime, earned=earned)
+    )
 
     await session.execute(stmt)
     await session.commit()
 
 
-async def check_record_salary(user_id: int, date: str) -> Salary:
+async def delete_record(data: Dict[str, str | int]) -> None:
     """
-    Функция для проверки существуют ли у нас запись для
-    данного пользователя или нет, чтобы избежать ситуации
-    когда у одного пользователя может быть множество записей с одной датой.
-    """
-    session: AsyncSession = await get_async_session()
-
-    stmt = (select(Salary)
-            .where(Salary.user_chat_id == user_id)
-            .where(Salary.date == date)
-            )
-
-    data = await session.scalars(stmt)
-    await session.close()
-    return data.first()
-
-
-async def get_salary(
-        year: str,
-        month: str,
-        user_id: int,
-        period: int,
-        session: AsyncSession
-) -> Row:
-    """Функция получает данные о зарплате по периодам."""
-    stmt = ((
-        select(
-            func.sum(Salary.earned),
-            func.sum(Salary.base_hours),
-            func.sum(Salary.overtime)
-        )
-        .where(Salary.date.like(f"%/{month}/{year}"))
-        .where(Salary.user_chat_id == user_id))
-        .where(Salary.period == period)
-        )
-    data = await session.execute(stmt)
-    return data.one()
-
-
-async def get_total_salary(
-        year: str, month: str, user_id: int
-) -> Tuple[Row, Row, Row]:
-    """
-    Функция для получения информации о зарплате по периодам и за целый месяц.
+    Функция для удаления записи за выбранное число.
+    :param data:
+    :return:
     """
     session: AsyncSession = await get_async_session()
-    period_one: Row = await get_salary(year, month, user_id, 1, session)
-    period_two: Row = await get_salary(year, month, user_id, 2, session)
-
-    stmt = ((
-        select(
-            func.sum(Salary.earned),
-            func.sum(Salary.base_hours),
-            func.sum(Salary.overtime)
-        )
-        .where(Salary.date.like(f"%/{month}/{year}"))
-        .where(Salary.user_chat_id == user_id))
-        )
-
-    data = await session.execute(stmt)
-    await session.close()
-    return period_one, period_two, data.one()
-
-
-async def delete_record(data):
-    session = await get_async_session()
-    stmt = (
+    parse_date: date = datetime.strptime(data["date"], "%Y-%m-%d")
+    stmt: Delete = (
         delete(Salary)
         .where(Salary.user_chat_id == data["user_id"])
-        .where(Salary.date == data["date"])
+        .where(Salary.date == parse_date)
     )
     await session.execute(stmt)
     await session.commit()
