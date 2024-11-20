b3d86d55c664b923f7b92d722cec42c0068ba53c
Maksim <m-zhitkov@inbox.ru>
2024-11-20 20:16:35 +0300

    Minor fixed

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ from decouple import config
 
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
@@ -12,6 +9,6 @@ DB_PASS = os.environ.get("DB_PASS")
 
 BOT_TOKEN = config("TOKEN")
 
-weekdays = ("Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс")
+weekdays: tuple = ("Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс")
 app_schedule: dict = {}
 scheduler_ids: dict = {}
