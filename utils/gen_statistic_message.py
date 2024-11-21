e57d8a29203e7ab0425453f57d5a6a1fc88fe290
Maksim <m-zhitkov@inbox.ru>
2024-11-21 21:55:03 +0300

    Corrected the calculation of statistics

--- utils/gen_statistic_message.py
+++ utils/gen_statistic_message.py
@@ -13,10 +13,6 @@ async def gen_message_statistic(data: Row[tuple], year: int) -> str:
     """
     total_hours: int = data[1] + data[2]
     days_in_year: int = 365
-    date_now: datetime = datetime.now()
-
-    if year == date_now.year:
-        days_in_year = date_now.timetuple().tm_year
 
     message: str = (
         f"Ваша статистика за {hbold(year)} год.\n"
@@ -26,7 +22,7 @@ async def gen_message_statistic(data: Row[tuple], year: int) -> str:
         f"Из них переработки: {hbold(data[2])}ч.\n"
         f"{60 * "-"}\n"
         f"За год вы потратили на работу {int(total_hours // 24)} дней,\n"
-        f"Что составляет {hbold(round(total_hours / (days_in_year * 24) * 100, 2))}% "
+        f"Что составляет {hbold(round((total_hours / (days_in_year * 24)) * 100, 2))}% "
         f"времени в году."
     )
     return message
