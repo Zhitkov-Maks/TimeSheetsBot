312823a2e1b2aba2d51ed5bc488c2d6e2f4d1ca3
Maksim <m-zhitkov@inbox.ru>
2024-12-01 16:24:13 +0300

    Implemented auxiliary functions for calculations for work on the selected day.

--- utils/current_day.py
+++ utils/current_day.py
@@ -1,31 +1,34 @@
 """Вспомогательный модуль для подсчета зарплаты за выбранный день."""
-
+from aiogram.utils.markdown import hbold
+from dataclasses import dataclass
 from typing_extensions import Tuple
 
 from crud.settings import get_settings_user_by_id
 from database import Settings
+from database.models import Salary
 
 
+@dataclass
 class Employee:
     """
     Класс нужен если пользователь, не сделал настройки под себя,
     тогда делаем значение по умолчанию.
     """
-
-    def __init__(self, price, overtime):
-        self.price = price
-        self.overtime = overtime
+    price: float
+    overtime: float
 
 
-async def earned_per_shift(base: float, overtime: float, user_id: int) -> int:
+async def earned_per_shift(base: float, overtime: float, user_id: int) -> float:
     """
     Формируем сумму, заработанную за смену.
+
     :param base: Базовая ставка.
     :param overtime: Доплата за переработку.
     :param user_id: Id юзера.
     :return: Заработанную сумму за месяц.
     """
     settings: Settings | Employee = await get_settings_user_by_id(user_id)
+
     if settings is None:
         settings = Employee(300, 100)
     return round(
@@ -44,5 +47,25 @@ async def earned_salary(
     :param user_id: Id пользователя.
     :return: Кортеж с временем, переработкой, заработком.
     """
-    earned: int = await earned_per_shift(time, overtime, user_id)
+    earned: float = await earned_per_shift(time, overtime, user_id)
     return time, overtime, earned
+
+
+async def gen_message_for_choice_day(salary: Salary, choice_date: str) -> str:
+    """
+    Генерируем простое сообщения по з/п за выбранную смену для пользователя.
+
+    :param choice_date: Переданная дата из календаря.
+    :param salary: Заработок за определенный день.
+    :return: Сообщение для пользователя.
+    """
+    if not salary:
+        return f"За дату {choice_date} нет данных."
+
+    other: float = salary.other_income if salary.other_income else 0
+    return (
+        f"Дата: {choice_date}. \nВы отработали: "
+        f"{hbold(salary.base_hours + salary.overtime)} часов.\n"
+        f"Заработали: {salary.earned + other:,.2f}₽.\n"
+        f"Из них прочие доходы: {other:,.2f}₽."
+    )
