c22463c44c5e10104123e9430c485c9174d1bdc6
Maksim <m-zhitkov@inbox.ru>
2024-12-01 17:33:28 +0300

    Implemented keyboards for working with the bot.

--- keywords/keyword.py
+++ keywords/keyword.py
@@ -1,23 +1,7 @@
-from typing import List, Dict
+from typing import List
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from database.models import Salary
-
-month_tuple: Dict[int, str] = {
-    1: "Январь",
-    2: "Февраль",
-    3: "Март",
-    4: "Апрель",
-    5: "Май",
-    6: "Июнь",
-    7: "Июль",
-    8: "Август",
-    9: "Сентябрь",
-    10: "Октябрь",
-    11: "Ноябрь",
-    12: "Декабрь",
-}
 
 cancel: List[List[InlineKeyboardButton]] = [
     [InlineKeyboardButton(text="Отмена", callback_data="main")]
@@ -30,71 +14,27 @@ confirm: List[List[InlineKeyboardButton]] = [
     ]
 ]
 
-choice_remind: List[List[InlineKeyboardButton]] = [
-    [
-        InlineKeyboardButton(text="Добавить", callback_data="add_remind"),
-        InlineKeyboardButton(text="Удалить", callback_data="remove"),
-    ],
-    [
-        InlineKeyboardButton(text="Изменить время",
-                             callback_data="change_remind"),
-        InlineKeyboardButton(text="Отмена", callback_data="main"),
-    ],
-]
-
-
-def get_menu_bot() -> List[List[InlineKeyboardButton]]:
-    return [
+menu_button: List[List[InlineKeyboardButton]] = [
         [
-            InlineKeyboardButton(text=f"Календарь",
-                                 callback_data="month_current"),
+            InlineKeyboardButton(
+                text=f"Календарь", callback_data="month_current"
+            ),
             InlineKeyboardButton(text="Прогноз", callback_data="prediction"),
             InlineKeyboardButton(text="Настройки", callback_data="settings"),
         ],
         [
-            InlineKeyboardButton(text="Напоминание", callback_data="remind"),
-            InlineKeyboardButton(text="Статистика за год",
-                                 callback_data="statistic"),
+            InlineKeyboardButton(
+                text="Проставить смены", callback_data="many_add"
+            ),
+            InlineKeyboardButton(
+                text="Статистика за год", callback_data="statistic"
+            ),
         ],
     ]
-
-
-async def menu() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(inline_keyboard=get_menu_bot())
-
-
-async def get_data_choices_day(salary: Salary) -> InlineKeyboardMarkup:
-    if not salary:
-        return InlineKeyboardMarkup(
-            inline_keyboard=[
-                [
-                    InlineKeyboardButton(text="Меню", callback_data="main"),
-                    InlineKeyboardButton(text="Добавить", callback_data="add"),
-                ]
-            ]
-        )
-    else:
-        return InlineKeyboardMarkup(
-            inline_keyboard=[
-                [
-                    InlineKeyboardButton(text="Удалить", callback_data="del"),
-                    InlineKeyboardButton(text="Меню", callback_data="main"),
-                    InlineKeyboardButton(text="Изменить",
-                                         callback_data="change"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        text="Добавить бонус(доплаты, акции).",
-                        callback_data="bonus")
-                ]
-            ]
-        )
 
 
 cancel_button: InlineKeyboardMarkup = InlineKeyboardMarkup(
     inline_keyboard=cancel)
 confirm_menu: InlineKeyboardMarkup = InlineKeyboardMarkup(
     inline_keyboard=confirm)
-remind_button: InlineKeyboardMarkup = InlineKeyboardMarkup(
-    inline_keyboard=choice_remind
-)
+menu: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=menu_button)
