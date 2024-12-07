4cd92f6cf405e62a0454b052c5b8047d72a6a40d
Maksim <m-zhitkov@inbox.ru>
2024-12-07 14:37:12 +0300

    Added a comment to the code

--- handlers/current_day.py
+++ handlers/current_day.py
@@ -7,7 +7,8 @@ from aiogram.types import CallbackQuery, Message
 
 from config import BOT_TOKEN
 from crud.create import delete_record, add_other_income
-from handlers.bot_answer import send_calendar_and_message, processing_data
+from handlers.bot_answer import send_calendar_and_message, processing_data, \
+    decorator_errors
 from keywords.keyword import cancel_button
 from loader import add_record_text
 from states.current_day import CreateState
@@ -18,6 +19,7 @@ bot = Bot(token=BOT_TOKEN)
 
 
 @create_router.callback_query(F.data.in_(["change", "add"]))
+@decorator_errors
 async def on_date_today(callback: CallbackQuery, state: FSMContext) -> None:
     """Обработчик команд для добавления или изменения записи о смене."""
     await state.update_data(action=callback.data, callback=callback.id)
@@ -27,9 +29,12 @@ async def on_date_today(callback: CallbackQuery, state: FSMContext) -> None:
 
 
 @create_router.message(CreateState.check_data)
+@decorator_errors
 async def check_data(message: types.Message, state: FSMContext) -> None:
     """
-    Сохранение и проверка введенного отработанного времени.
+    Сохранение и проверка введенного отработанного времени. Если проверка
+    проходит, то отправляем на расчет заработка и добавление записи в бд.
+    А затем заново открывает календарь.
     """
     await state.update_data(user_id=message.from_user.id)
     data: Dict[str, str | float] = await state.get_data()
@@ -52,8 +57,15 @@ async def check_data(message: types.Message, state: FSMContext) -> None:
 
 
 @create_router.callback_query(F.data == "bonus")
-async def add_other_surcharges(callback: CallbackQuery,
-                               state: FSMContext) -> None:
+@decorator_errors
+async def add_other_surcharges(
+        callback: CallbackQuery,
+        state: FSMContext
+) -> None:
+    """
+    Обработчик для добавления прочих доходов к выбранному дню. Просит ввести
+    пользователя сумму прочего дохода.
+    """
     await state.set_state(CreateState.other_income)
     await callback.message.answer(
         text="Введите сумму прочего дохода, сюда можно добавить например "
@@ -62,13 +74,23 @@ async def add_other_surcharges(callback: CallbackQuery,
 
 
 @create_router.message(CreateState.other_income)
+@decorator_errors
 async def update_other_income(message: Message, state: FSMContext):
+    """
+    Обработчик проверяет введенное пользователем число, если все хорошо,
+    то отправляет на сохранение прочего дохода в бд, и формирует заново
+    календарь за тот месяц, где было добавление.
+    """
     try:
         income: float = float(message.text)
         await state.update_data(user_id=message.from_user.id)
         data: dict = await state.get_data()
         await add_other_income(income, data)
+        await message.answer(text=f"Прочий доход в размере "
+                                  f"{float(message.text):,.2f} "
+                                  f"был успешно добавлен!")
         await send_calendar_and_message(message.from_user.id, data, state)
+
     except ValueError:
         await message.reply(
             "Ошибка ввода, необходимо вводить либо целые числа, "
@@ -77,6 +99,7 @@ async def update_other_income(message: Message, state: FSMContext):
 
 
 @create_router.callback_query(F.data == "del")
+@decorator_errors
 async def delete_record(callback: CallbackQuery, state: FSMContext) -> None:
     """Отправка на удаление записи."""
     await state.update_data(user_id=callback.from_user.id)
