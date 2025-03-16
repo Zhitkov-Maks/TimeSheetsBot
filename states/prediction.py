77a29d76058c3ea932cf59aca3d49c462153f4a2
Maksim <m-zhitkov@inbox.ru>
2025-03-16 11:16:50 +0300

    Removed unnecessary functionality.

--- states/prediction.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class Prediction(StatesGroup):
-    """
-    Класс для определения состояний, связанных с прогнозированием з/п.
-    Используется для управления состоянием выбора месяца для прогнозирования.
-    """
-    month: State = State()  # Состояние для выбора месяца
